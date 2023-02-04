33c13bf1a9795d53af64897985eed02e3c190e91
Danil Mosolov <koti4ch@gmail.com>
2023-02-04 09:40:51 +0500

    feat/created best_path struct

---
+++ pkg/algo/best_path.go
@@ -0,0 +1,7 @@
+package algo
+
+// BestPath contains the solution of the most optimal path
+type BestPath struct {
+	Distance int64
+	Path     []int
+}
---
+++ pkg/algo/best_paths.go
@@ -0,0 +1,4 @@
+package algo
+
+// BestPaths contains the list of best solutions
+type BestPaths []BestPath
